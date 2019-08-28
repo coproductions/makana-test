63b3b1eb29c31c41a00fcbae0c7eaf8fa94a543e
coproductions <constantin.dumba@gmail.com>
2019-08-28 07:22:08 -0700

    adds appbar

--- app/src/components/HomePage.js
+++ app/src/components/HomePage.js
@@ -5,22 +5,26 @@ import FeedData from '../containers/FeedData';
 import FeedSubscriptionData from '../containers/FeedSubscriptionData';
 import ListComments from './ListComments';
 import Notice from './Notice';
+import AppBar from './AppBar';
 
 const styles = theme => ({
   page: {
     margin: theme.spacing(1),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 });
 
 const enhanced = compose(withStyles(styles));
 
 export default enhanced(({ classes }) => (
-  <div className={classes.page}>
-    <FeedSubscriptionData>{props => <Notice {...props} />}</FeedSubscriptionData>
+  <React.Fragment>
+    <AppBar />
+    <div className={classes.page}>
+      <FeedSubscriptionData>{props => <Notice {...props} />}</FeedSubscriptionData>
 
-    <FeedData>{props => <ListComments {...props} />}</FeedData>
-  </div>
+      <FeedData>{props => <ListComments {...props} />}</FeedData>
+    </div>
+  </React.Fragment>
 ));
