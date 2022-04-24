4e95e476a43f09e66a27b98034285488f51b0290
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-24 20:58:56 +0800

    Delete app directory

--- app/src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export * from './useAuthMutation';
-export * from './useUserQuery';
-export * from './useFeedSubscription';
-export * from './useErrorHandler';
--- app/src/operations/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Queries';
-export * from './Mutations';
-export * from './Subscriptions';
